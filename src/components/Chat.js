2018eaf4f353db48267bcb892dbd3074bc3cc53f
Vector73 <v.shm.kunal@gmail.com>
2023-12-26 12:38:47 +0530

    add chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -6,7 +6,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addMessage } from '../features/messagesSlice';
 import { socket } from '../socket';
 import { FaArrowRight } from 'react-icons/fa';
-import { Howl } from 'howler';
 
 export default function Chat(props) {
     const [message, setMessage] = useState('');
